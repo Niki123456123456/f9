1191ba69975b714ce529a95f08555a1bd9b5e329
Niki123456123456 <niklasnordkamp@web.de>
2023-06-30 15:38:06 +0200

    add rasterizer

--- src/app.rs
+++ src/app.rs
@@ -1,8 +1,9 @@
 use eframe::{
     egui_wgpu::wgpu::util::DeviceExt,
     egui_wgpu::{self, wgpu, RenderState},
-    wgpu::{AdapterInfo, Device},
+    wgpu::{AdapterInfo, BindGroupLayout, BufferDescriptor, BufferUsages, Device},
 };
+use egui::{epaint::Shadow, Color32, Margin, Rounding, Stroke, Vec2};
 use instant::{Duration, Instant};
 use std::{num::NonZeroU64, sync::Arc};
 
@@ -14,7 +15,11 @@ pub struct TemplateApp {
     #[serde(skip)]
     last_render_time: Instant,
     #[serde(skip)]
-    cp : Option<ComputeResources>,
+    cp: Option<ComputeResources>,
+    #[serde(skip)]
+    raster: Option<RasterResources>,
+    #[serde(skip)]
+    screen: Option<TriangleRenderResources>,
 }
 
 impl Default for TemplateApp {
@@ -23,8 +28,227 @@ impl Default for TemplateApp {
             adapter_info: None,
             last_render_time: Instant::now(),
             cp: None,
+            raster: None,
+            screen: None,
         }
     }
+}
+
+fn build_screen_pass(
+    device: &Arc<Device>,
+    wgpu_render_state: &RenderState,
+    outputcolor_buffer: &wgpu::Buffer,
+) -> TriangleRenderResources2 {
+    let label = Some("screen_pass");
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        label,
+        entries: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStages::FRAGMENT,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+        ],
+    });
+
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label,
+        source: wgpu::ShaderSource::Wgsl(include_str!("./shaders/fullscreenQuad.wgsl").into()),
+    });
+
+    let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label,
+        layout: Some(
+            &device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            }),
+        ),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vert_main",
+            buffers: &[],
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: "frag_main",
+            targets: &[Some(wgpu_render_state.target_format.into())],
+        }),
+        primitive: wgpu::PrimitiveState::default(),
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        multiview: None,
+    });
+
+    let uniform_buffersize = 4 * 2;
+    let uniform_buffer = device.create_buffer(&BufferDescriptor {
+        label,
+        size: uniform_buffersize,
+        usage: BufferUsages::COPY_DST | BufferUsages::UNIFORM,
+        mapped_at_creation: false,
+    });
+
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label,
+        layout: &bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: uniform_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: outputcolor_buffer.as_entire_binding(),
+            },
+        ],
+    });
+
+    return TriangleRenderResources2 {
+        pipeline,
+        bind_group,
+        uniform_buffer,
+    };
+}
+
+fn build_raster_pass(device: &Arc<Device>, wgpu_render_state: &RenderState) -> RasterResources {
+    let label = Some("raster_pass");
+
+    let vertex_buffer = device.create_buffer(&BufferDescriptor {
+        label,
+        size: 4 * 6,
+        usage: BufferUsages::STORAGE,
+        mapped_at_creation: false,
+    });
+
+    let width = 3000;
+    let height = 3000;
+    let color_channels = 3;
+    let outputcolor_buffersize = 4 * (width * height) * color_channels;
+    let outputcolor_buffer = device.create_buffer(&BufferDescriptor {
+        label,
+        size: outputcolor_buffersize,
+        usage: BufferUsages::STORAGE | BufferUsages::COPY_SRC,
+        mapped_at_creation: false,
+    });
+
+    let uniform_buffersize = 4 * 2 + 4 * 16 + 8;
+    let uniform_buffer = device.create_buffer(&BufferDescriptor {
+        label,
+        size: uniform_buffersize,
+        usage: BufferUsages::COPY_DST | BufferUsages::UNIFORM,
+        mapped_at_creation: false,
+    });
+
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        label,
+        entries: &[
+            wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 1,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+            wgpu::BindGroupLayoutEntry {
+                binding: 2,
+                visibility: wgpu::ShaderStages::COMPUTE,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            },
+        ],
+    });
+
+    let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        label,
+        layout: &bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: outputcolor_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: vertex_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: uniform_buffer.as_entire_binding(),
+            },
+        ],
+    });
+
+    let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label,
+        source: wgpu::ShaderSource::Wgsl(include_str!("./shaders/computeRasterizer.wgsl").into()),
+    });
+
+    let raster_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label,
+        layout: Some(
+            &device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            }),
+        ),
+        module: &shader,
+        entry_point: "main",
+    });
+
+    let clear_pipeline = device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor {
+        label,
+        layout: Some(
+            &device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            }),
+        ),
+        module: &shader,
+        entry_point: "clear",
+    });
+
+    return RasterResources {
+        clear_pipeline,
+        raster_pipeline,
+        bind_group,
+        uniform_buffer,
+        vertex_buffer,
+        outputcolor_buffer,
+    };
 }
 
 fn build_compute_shader(device: &Arc<Device>, wgpu_render_state: &RenderState) -> ComputeResources {
@@ -60,7 +284,6 @@ fn build_compute_shader(device: &Arc<Device>, wgpu_render_state: &RenderState) -
         module: &shader,
         entry_point: "main",
     });
-
 
     let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label,
@@ -110,17 +333,15 @@ fn build_test_shader(
         }],
     });
 
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-        label,
-        bind_group_layouts: &[&bind_group_layout],
-        push_constant_ranges: &[],
-    });
-
-    //device.create_compute_pipeline(&wgpu::ComputePipelineDescriptor { label: (), layout: (), module: (), entry_point: () })
-
     let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label,
-        layout: Some(&pipeline_layout),
+        layout: Some(
+            &device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label,
+                bind_group_layouts: &[&bind_group_layout],
+                push_constant_ranges: &[],
+            }),
+        ),
         vertex: wgpu::VertexState {
             module: &shader,
             entry_point: "vs_main",
@@ -172,7 +393,11 @@ impl TemplateApp {
             .paint_callback_resources
             .insert(build_test_shader(device, wgpu_render_state));
 
-        
+            wgpu_render_state
+            .renderer
+            .write()
+            .paint_callback_resources
+            .insert(WindowSize{ width: 1000.0, height: 1000.0 });
 
         let mut app: TemplateApp = if let Some(storage) = cc.storage {
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
@@ -180,8 +405,20 @@ impl TemplateApp {
             Default::default()
         };
 
+        let raster = build_raster_pass(device, wgpu_render_state);
+        let screen_pass = build_screen_pass(device, wgpu_render_state, &raster.outputcolor_buffer);
+
+        wgpu_render_state
+            .renderer
+            .write()
+            .paint_callback_resources
+            .insert(screen_pass);
+
+           
         app.adapter_info = Some(wgpu_render_state.adapter.get_info());
-        app.cp= Some(build_compute_shader(device, wgpu_render_state));
+        app.cp = Some(build_compute_shader(device, wgpu_render_state));
+        app.raster = Some(raster);
+        //app.screen = Some(screen_pass);
         return app;
     }
 }
@@ -192,55 +429,60 @@ impl eframe::App for TemplateApp {
     }
 
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        if let Some(cp) = & self.cp {
-            let wstate = _frame.wgpu_render_state().unwrap();
-           
-            let mut encoder = wstate.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Compute Encoder"),
+        
+        egui::CentralPanel::default()
+            .frame(egui::Frame {
+                inner_margin: Margin::same(0.),
+                outer_margin: Margin::same(0.),
+                rounding: Rounding::none(),
+                shadow: Shadow::NONE,
+                fill: Color32::TRANSPARENT,
+                stroke: Stroke::NONE,
+            })
+            .show(ctx, |ui| {
+                let mut spacing = ui.spacing_mut();
+                spacing.item_spacing = Vec2::ZERO;
+
+                let (rect, response) =
+                    ui.allocate_at_least(ui.available_size(), egui::Sense::drag());
+                let renderstate = _frame.wgpu_render_state().unwrap();
+                {
+                    let mut writer = renderstate.renderer.write();
+                    let s : &mut WindowSize = writer.paint_callback_resources.get_mut().unwrap();
+                    if s.width != rect.width() || s.height != rect.height() {
+                        s.width = rect.width();
+                        s.height = rect.height();
+                        println!("{} {}", s.width, s.height);
+                    }
+                }
+                if let Some(cp) = &self.raster {
+                    cp.execute(renderstate, rect.size());
+                }
+                
+
+                
+                let cb = egui_wgpu::CallbackFn::new()
+                    .prepare(move |device, queue, _encoder, paint_callback_resources| {
+                        let resources: &TriangleRenderResources2 =
+                            paint_callback_resources.get().unwrap();
+                            let size: &WindowSize =
+                            paint_callback_resources.get().unwrap();
+                        resources.set_size(device, queue, size.width, size.height);
+                        Vec::new()
+                    })
+                    .paint(move |_info, render_pass, paint_callback_resources| {
+                        let resources: &TriangleRenderResources2 =
+                            paint_callback_resources.get().unwrap();
+                        resources.paint2(render_pass);
+                    });
+
+                let callback = egui::PaintCallback {
+                    rect,
+                    callback: Arc::new(cb),
+                };
+
+                ui.painter().add(callback);
             });
-            
-            {
-                let mut compute_pass = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                    label: Some("Compute Pass"),
-                });
-                compute_pass.set_pipeline(&cp.pipeline);
-                compute_pass.set_bind_group(0, &cp.bind_group, &[]);
-                compute_pass.dispatch_workgroups(4, 1, 1);
-            
-            }
-            
-            let command_buffer = encoder.finish();
-            let i = wstate.queue.submit(Some(command_buffer));
-        }
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("f9");
-
-            let (rect, response) =
-                ui.allocate_exact_size(egui::Vec2::splat(300.0), egui::Sense::drag());
-
-            
-            
-
-            let cb = egui_wgpu::CallbackFn::new()
-                .prepare(move |device, queue, _encoder, paint_callback_resources| {
-                    let resources: &TriangleRenderResources =
-                        paint_callback_resources.get().unwrap();
-                    resources.prepare(device, queue, 0.0);
-                    Vec::new()
-                })
-                .paint(move |_info, render_pass, paint_callback_resources| {
-                    let resources: &TriangleRenderResources =
-                        paint_callback_resources.get().unwrap();
-                    resources.paint(render_pass);
-                });
-
-            let callback = egui::PaintCallback {
-                rect,
-                callback: Arc::new(cb),
-            };
-
-            ui.painter().add(callback);
-        });
 
         if let Some(adapter_info) = &self.adapter_info {
             egui::Window::new("GPU Info").show(&ctx, |ui| {
@@ -261,10 +503,71 @@ impl eframe::App for TemplateApp {
         ctx.request_repaint();
     }
 }
+
+struct WindowSize{
+    width : f32,
+    height: f32,
+}
+
+struct RasterResources {
+    clear_pipeline: wgpu::ComputePipeline,
+    raster_pipeline: wgpu::ComputePipeline,
+    bind_group: wgpu::BindGroup,
+    uniform_buffer: wgpu::Buffer,
+    vertex_buffer: wgpu::Buffer,
+    outputcolor_buffer: wgpu::Buffer,
+}
+
 struct ComputeResources {
     pipeline: wgpu::ComputePipeline,
     bind_group: wgpu::BindGroup,
     buffer: wgpu::Buffer,
+}
+
+impl RasterResources {
+    fn execute(&self, renderstate: &RenderState, size : Vec2) {
+        renderstate.queue.write_buffer(
+            &self.uniform_buffer,
+            0,
+            bytemuck::cast_slice(&[size.x, size.y]),
+        );
+
+
+        let mut encoder =
+            renderstate
+                .device
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some("clear Encoder"),
+                });
+
+        {
+            let mut passencoder: wgpu::ComputePass<'_> =
+                encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                    label: Some("clear pass"),
+                });
+            passencoder.set_pipeline(&self.clear_pipeline);
+            passencoder.set_bind_group(0, &self.bind_group, &[]);
+            let work_group_count = ((size.x as f32) * (size.y as f32) / (256 as f32)).ceil() as u32;
+            passencoder.dispatch_workgroups(work_group_count, 1, 1);
+        }
+        renderstate.queue.submit(Some(encoder.finish()));
+
+        let mut encoder =
+            renderstate
+                .device
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some("Raster Encoder"),
+                });
+        {
+            let mut passencoder = encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                label: Some("raster pass"),
+            });
+            passencoder.set_pipeline(&self.raster_pipeline);
+            passencoder.set_bind_group(0, &self.bind_group, &[]);
+            passencoder.dispatch_workgroups(1, 1, 1);
+        }
+        renderstate.queue.submit(Some(encoder.finish()));
+    }
 }
 
 struct TriangleRenderResources {
@@ -272,8 +575,27 @@ struct TriangleRenderResources {
     bind_group: wgpu::BindGroup,
     uniform_buffer: wgpu::Buffer,
 }
+struct TriangleRenderResources2 {
+    pipeline: wgpu::RenderPipeline,
+    bind_group: wgpu::BindGroup,
+    uniform_buffer: wgpu::Buffer,
+}
+impl TriangleRenderResources2 {
+    fn set_size(&self, _device: &wgpu::Device, queue: &wgpu::Queue, width: f32, height: f32) {
+        // Update our uniform buffer with the angle from the UI
+        queue.write_buffer(
+            &self.uniform_buffer,
+            0,
+            bytemuck::cast_slice(&[width, height]),
+        );
+    }
 
-
+    fn paint2<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
+        render_pass.set_pipeline(&self.pipeline);
+        render_pass.set_bind_group(0, &self.bind_group, &[]);
+        render_pass.draw(0..6, 0..1);
+    }
+}
 
 impl TriangleRenderResources {
     fn prepare(&self, _device: &wgpu::Device, queue: &wgpu::Queue, angle: f32) {
@@ -285,8 +607,7 @@ impl TriangleRenderResources {
         );
     }
 
-    fn paint<'rp>(&'rp self, render_pass: &mut wgpu::RenderPass<'rp>) {
-        // Draw our triangle!
+    fn paint<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
         render_pass.set_pipeline(&self.pipeline);
         render_pass.set_bind_group(0, &self.bind_group, &[]);
         render_pass.draw(0..3, 0..1);
---
+++ src/shaders/computeRasterizer.wgsl
@@ -0,0 +1,147 @@
+struct ColorBuffer {
+  values: array<atomic<u32>>,
+};
+
+struct UBO {
+  screenWidth: f32,
+  screenHeight: f32,
+  modelViewProjectionMatrix: mat4x4<f32>,
+};
+
+struct Vertex { x: f32, y: f32, z: f32, };
+
+struct VertexBuffer {
+  values: array<Vertex>,
+};
+
+@group(0) @binding(0) var<storage, read_write> outputColorBuffer : ColorBuffer;
+@group(0) @binding(1) var<storage, read> vertexBuffer : VertexBuffer;
+@group(0) @binding(2) var<uniform> uniforms : UBO;
+
+// From: https://github.com/ssloy/tinyrenderer/wiki/Lesson-2:-Triangle-rasterization-and-back-face-culling
+fn barycentric(v1: vec3<f32>, v2: vec3<f32>, v3: vec3<f32>, p: vec2<f32>) -> vec3<f32> {
+  let u = cross(
+    vec3<f32>(v3.x - v1.x, v2.x - v1.x, v1.x - p.x), 
+    vec3<f32>(v3.y - v1.y, v2.y - v1.y, v1.y - p.y)
+  );
+
+  if (abs(u.z) < 1.0) {
+    return vec3<f32>(-1.0, 1.0, 1.0);
+  }
+
+  return vec3<f32>(1.0 - (u.x+u.y)/u.z, u.y/u.z, u.x/u.z); 
+}
+
+fn get_min_max(v1: vec3<f32>, v2: vec3<f32>, v3: vec3<f32>) -> vec4<f32> {
+  var min_max = vec4<f32>();
+  min_max.x = min(min(v1.x, v2.x), v3.x);
+  min_max.y = min(min(v1.y, v2.y), v3.y);
+  min_max.z = max(max(v1.x, v2.x), v3.x);
+  min_max.w = max(max(v1.y, v2.y), v3.y);
+
+  return min_max;
+}
+
+fn color_pixel(x: u32, y: u32, r: u32, g: u32, b: u32) {
+  let pixelID = u32(x + y * u32(1000.0)) * 3u;
+  
+  atomicMin(&outputColorBuffer.values[pixelID + 0u], r);
+  atomicMin(&outputColorBuffer.values[pixelID + 1u], g);
+  atomicMin(&outputColorBuffer.values[pixelID + 2u], b);
+}
+
+fn draw_triangle(v1: vec3<f32>, v2: vec3<f32>, v3: vec3<f32>) {
+  let min_max = get_min_max(v1, v2, v3);
+  let startX = u32(min_max.x);
+  let startY = u32(min_max.y);
+  let endX = u32(min_max.z);
+  let endY = u32(min_max.w);
+
+  for (var x: u32 = startX; x <= endX; x = x + 1u) {
+    for (var y: u32 = startY; y <= endY; y = y + 1u) {
+      let bc = barycentric(v1, v2, v3, vec2<f32>(f32(x), f32(y))); 
+      let color = (bc.x * v1.z + bc.y * v2.z + bc.z * v3.z) * 50.0 - 400.0;
+
+      let R = color;
+      let G = color;
+      let B = color;
+/*
+      if (bc.x < 0.0 || bc.y < 0.0 || bc.z < 0.0) {
+        continue;
+      }*/
+      color_pixel(x, y, 200u, 200u, 200u);
+    }
+  }
+}
+
+fn draw_line(v1: vec3<f32>, v2: vec3<f32>) {
+  let v1Vec = vec2<f32>(v1.x, v1.y);
+  let v2Vec = vec2<f32>(v2.x, v2.y);
+
+  let dist = i32(distance(v1Vec, v2Vec));
+  for (var i = 0; i < dist; i = i + 1) {
+    let x = u32(v1.x + f32(v2.x - v1.x) * (f32(i) / f32(dist)));
+    let y = u32(v1.y + f32(v2.y - v1.y) * (f32(i) / f32(dist)));
+    color_pixel(x, y, 255u, 255u, 255u);
+  }
+}
+
+fn project(v: Vertex) -> vec3<f32> {
+  var screenPos = uniforms.modelViewProjectionMatrix * vec4<f32>(v.x, v.y, v.z, 1.0);
+  screenPos.x = (screenPos.x / screenPos.w) * uniforms.screenWidth;
+  screenPos.y = (screenPos.y / screenPos.w) * uniforms.screenHeight;
+
+  return vec3<f32>(screenPos.x, screenPos.y, screenPos.w);
+}
+
+fn is_off_screen(v: vec3<f32>) -> bool {
+  if (v.x < 0.0 || v.x > 1000.0 || v.y < 0.0 || v.y > 1000.0 ) {
+    return true;
+  }
+
+  return false;
+}
+/*
+@compute @workgroup_size(256, 1)
+fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
+  let index = global_id.x * 3u;
+
+  let v1 = vec3<f32>(10.0, 10.0, 1.0); // project(vertexBuffer.values[index + 0u]);
+  let v2 = vec3<f32>(10.0, 20.0, 1.0); //project(vertexBuffer.values[index + 1u]);
+  let v3 = vec3<f32>(20.0, 10.0, 1.0); //project(vertexBuffer.values[index + 2u]);
+/*
+  if (is_off_screen(v1) || is_off_screen(v2) || is_off_screen(v3)) {
+    return;
+  }*/
+
+  draw_triangle(v1, v2, v3);
+
+  for (var i = 0u; i < 1000u; i = i + 1u) {
+     atomicStore(&outputColorBuffer.values[i + 0u], 255u);
+    atomicStore(&outputColorBuffer.values[i + 1u], 255u);
+    atomicStore(&outputColorBuffer.values[i + 2u], 255u);
+  }
+}*/
+@compute @workgroup_size(1, 1) //@workgroup_size(256, 1)
+fn main(@builtin(global_invocation_id) global_id : vec3<u32>) {
+
+  for (var x = 50u; x < 100u; x = x + 1u) {
+    for (var y = 50u; y < 100u; y = y + 1u) {
+      
+      atomicStore(&outputColorBuffer.values[(x + y * u32(uniforms.screenWidth)) * 3u + 0u], 255u);
+      atomicStore(&outputColorBuffer.values[(x + y * u32(uniforms.screenWidth)) * 3u + 1u], 255u);
+      atomicStore(&outputColorBuffer.values[(x + y * u32(uniforms.screenWidth)) * 3u + 2u], 255u);
+    }
+  }
+
+  
+}
+
+@compute @workgroup_size(256, 1)
+fn clear(@builtin(global_invocation_id) global_id : vec3<u32>) {
+  let index = global_id.x * 3u;
+
+  atomicStore(&outputColorBuffer.values[index + 0u], 0u);
+  atomicStore(&outputColorBuffer.values[index + 1u], 0u);
+  atomicStore(&outputColorBuffer.values[index + 2u], 255u);
+}
